da04faab541123c53c6fd1d20bdd1046562e72b0
Laís <lais.v@lyncas.net>
2022-01-27 15:28:57 -0300

    configurando testes

--- testeAsync.py
+++ testeAsync.py
@@ -9,18 +9,18 @@ def fetch(session, dados):
     base_url = "https://marcapontoapi.azurewebsites.net/BatePonto/Marcar"
     with session.post(base_url + dados) as response:
         data = response.text
-        print(data, dados)
+        # print(data, dados)
         if response.status_code != 200:
-            print("FAILURE::{0}".format(base_url + dados))
+            print("ERRO::{0}".format(base_url + dados))
 
         elapsed = default_timer() - START_TIME
         time_completed_at = "{:5.2f}s".format(elapsed)
-        print("SUCCESS::{0:<30} {1:>20}".format(dados, time_completed_at))
+        print("SUCESSO::{0:<30} {1:>20}".format(dados, time_completed_at))
 
         return data
 
 async def get_data_asynchronous():
-    print("{0:<30} {1:>20}".format("File", "Completed at"))
+    print("{0:<30} {1:>20}".format("Parâmetros", "Terminou em"))
     with ThreadPoolExecutor(max_workers=5) as executor:
         with requests.Session() as session:
             loop = asyncio.get_event_loop()
@@ -31,7 +31,7 @@ async def get_data_asynchronous():
                     fetch,
                     *(session, "?IncludedAt=2021-01-01&EmployeeId="+ str(i) +"&EmployerId="+ str(i))
                 )
-                for i in range(1, 51)
+                for i in range(1, 11)
             ]
             
             for response in await asyncio.gather(*tasks):
